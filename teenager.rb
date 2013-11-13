f5a92b2fbf68c48236c9cd16162d01e669e4253a
Derek S <dereksilverman@gmail.com>
2013-11-13 12:25:07 -0500

    Test Success

---
+++ teenager.rb
@@ -0,0 +1,13 @@
+#Write a method called is_a_teenager that takes age as a parameter and returns
+#true/false depending on whether the age indicates that the person is a 
+#teenager
+
+#To test your solution run `rspec teenager_spec.rb` in your terminal
+
+def is_a_teenager? age
+	if age > 13 && age < 19
+		true
+	else
+		false
+	end
+end
